94e2d21705999659323d3c97bd5f238f4d476f64
Andrew Kelley <superjoe30@gmail.com>
2012-12-05 16:15:57 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+See [node-plan](https://github.com/superjoe30/node-plan)
+
+### input
+
+  * `callbackUrl` - the URL to hit when the task is run
+  * `callbackData` - this data is POSTed to the url as application/json
+
+### output
+
+none
+
+### options
+
+none
+
+### exports
+
+  * `url` - copied directly from input
---
+++ index.js
@@ -0,0 +1,54 @@
+var http = require('http')
+  , https = require('https')
+  , parseUrl = require('url').parse
+
+module.exports = {
+  start: function(done) {
+    var self = this;
+    
+    // consume the callback url
+    self.exports.url = self.context.callbackUrl;
+    delete self.context.callbackUrl;
+
+    if (! this.exports.url) {
+      done(new Error("context missing callbackUrl"));
+      return;
+    }
+
+    self.emit('update');
+
+    var payload;
+    try {
+      payload = JSON.stringify(self.context.callbackData);
+    } catch (err) {
+      done(err);
+      return;
+    }
+
+    var opts = parseUrl(self.exports.url);
+    opts.method = 'POST';
+    opts.agent = false;
+    opts.headers = {
+      'content-length': payload.length,
+      'content-type': 'application/json',
+    };
+    var httpModule = opts.protocol === 'https:' ? https : http;
+    var req = httpModule.request(opts, function(resp) {
+      resp.on('error', function(err) {
+        done(new Error("POST " + self.exports.url + ": " + err.stack));
+      });
+      resp.on('end', function() {
+        if (resp.statusCode === 200) {
+          done();
+        } else {
+          done(new Error(self.exports.url + " returned http code " + resp.statusCode));
+        }
+      });
+    });
+    req.on('error', function(err) {
+      done(new Error("unable to POST " + self.exports.url + ": " + err.stack));
+    });
+    req.write(payload);
+    req.end();
+  }
+};
